377d2148739fdcdf1bf6eef62556cab433dfbf9d
KausaChangs <changayakausa@gmail.com>
2023-10-10 08:18:24 +0200

    completed the start peices array

--- app.js
+++ app.js
@@ -6,4 +6,29 @@ const infoDisplay = document.querySelector("#info-display");
 
 const width = 8;
 
-const startPeices = [];
+const startPeices = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+];
